1fa6b559f711597c8c19c0ae6da73eedc7690284
PriyadarshanA-coder <priyadarshan.ayyappan@gmail.com>
2025-09-22 18:36:56 +0530

    Finished the Login Screen

---
+++ lib/Screens/login_screen.dart
@@ -0,0 +1,207 @@
+import 'package:ecommerce/Widgets/text_form.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final email = TextEditingController();
+  final password = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    // Screen size
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.05),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: screenHeight * 0.03),
+                Text(
+                  "Login to your account",
+                  style: GoogleFonts.poppins(
+                    fontSize: screenWidth * 0.08, // responsive font
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                SizedBox(height: screenHeight * 0.01),
+                Text(
+                  "It's great to see you again.",
+                  style: GoogleFonts.poppins(
+                    fontSize: screenWidth * 0.045,
+                    fontWeight: FontWeight.w200,
+                  ),
+                ),
+                SizedBox(height: screenHeight * 0.03),
+                Text(
+                  "Email",
+                  style: GoogleFonts.poppins(
+                    fontSize: screenWidth * 0.05,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                SizedBox(height: screenHeight * 0.01),
+                TextForm(
+                  controller: email,
+                  hintText: 'Enter your email address',
+                  obscureText: false,
+                ),
+                SizedBox(height: screenHeight * 0.02),
+                Text(
+                  "Password",
+                  style: GoogleFonts.poppins(
+                    fontSize: screenWidth * 0.05,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                SizedBox(height: screenHeight * 0.01),
+                TextForm(
+                  controller: password,
+                  hintText: 'Enter your password',
+                  obscureText: true,
+                ),
+                SizedBox(height: screenHeight * 0.01),
+                Row(
+                  children: [
+                    Text(
+                      "Forgot your password? ",
+                      style: GoogleFonts.poppins(
+                        fontSize: screenWidth * 0.038,
+                        fontWeight: FontWeight.w300,
+                      ),
+                    ),
+                    Text(
+                      "Reset your password",
+                      style: GoogleFonts.poppins(
+                        fontSize: screenWidth * 0.038,
+                        fontWeight: FontWeight.w400,
+                        decoration: TextDecoration.underline,
+                        decorationColor: Colors.black,
+                        decorationThickness: 1,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: screenHeight * 0.04),
+                InkWell(
+                  onTap: () {
+                    context.go('/catalogscreen');
+                  },
+                  child: Container(
+                    height: screenHeight * 0.07,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      color: Colors.black,
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Login",
+                        style: GoogleFonts.poppins(
+                          color: Colors.white,
+                          fontSize: screenWidth * 0.045,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: screenHeight * 0.03),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Divider(color: Colors.grey.shade300, thickness: 1),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: screenWidth * 0.02,
+                      ),
+                      child: Text(
+                        "Or",
+                        style: GoogleFonts.poppins(
+                          fontSize: screenWidth * 0.04,
+                          fontWeight: FontWeight.w300,
+                          color: Colors.grey.shade400,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Divider(color: Colors.grey.shade300, thickness: 1),
+                    ),
+                  ],
+                ),
+                SizedBox(height: screenHeight * 0.03),
+                InkWell(
+                  onTap: () {},
+                  child: Container(
+                    height: screenHeight * 0.065,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      color: Colors.transparent,
+                      border: Border.all(color: Colors.grey.shade300),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset("assets/images/google.png"),
+                        SizedBox(width: screenWidth * 0.03),
+                        Text(
+                          "Login with Google",
+                          style: GoogleFonts.poppins(
+                            fontSize: screenWidth * 0.042,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(height: screenHeight * 0.015),
+                InkWell(
+                  onTap: () {},
+                  child: Container(
+                    height: screenHeight * 0.065,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      color: Color(0xFF1877F2),
+                      border: Border.all(color: Colors.grey.shade300),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset("assets/images/facebook.png"),
+                        SizedBox(width: screenWidth * 0.03),
+                        Text(
+                          "Login with Facebook",
+                          style: GoogleFonts.poppins(
+                            fontSize: screenWidth * 0.042,
+                            fontWeight: FontWeight.w400,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(height: screenHeight * 0.03),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
