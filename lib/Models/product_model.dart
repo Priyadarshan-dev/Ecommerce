eee4b5c2887c0f25ff577c51b38b20e28943edf1
PriyadarshanA-coder <priyadarshan.ayyappan@gmail.com>
2025-09-22 18:44:13 +0530

    used Placeholder model

---
+++ lib/Models/product_model.dart
@@ -0,0 +1,13 @@
+class Product {
+  final String name;
+
+  Product({required this.name});
+}
+
+// Hardcoded product list
+final List<Product> products = [
+  Product(name: "Blue Tshirt"),
+  Product(name: "Red Jeans"),
+  Product(name: "White Shoes"),
+  Product(name: "Green Tshirt"),
+];
