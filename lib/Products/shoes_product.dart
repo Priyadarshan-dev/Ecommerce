3c9aa4de93305d6fbc6688bf36d3faae5223fc49
PriyadarshanA-coder <priyadarshan.ayyappan@gmail.com>
2025-09-24 19:12:19 +0530

    Worked on Product Detail Screen

--- lib/Products/shoes_product.dart
+++ lib/Products/shoes_product.dart
@@ -1,3 +1,5 @@
+import 'package:ecommerce/Models/product_model.dart';
+import 'package:ecommerce/Screens/product_detail.dart';
 import 'package:flutter/material.dart';
 
 class ShoesProducts extends StatelessWidget {
@@ -6,7 +8,7 @@ class ShoesProducts extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridView.builder(
-      itemCount: 1,
+      itemCount: products.length,
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
         mainAxisSpacing: 10,
@@ -14,12 +16,26 @@ class ShoesProducts extends StatelessWidget {
         childAspectRatio: 0.7,
       ),
       itemBuilder: (context, index) {
-        return Container(
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.grey.shade300),
-            borderRadius: BorderRadius.circular(12),
+        final product = products[index];
+        return InkWell(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (_) => ProductDetailScreen(product: product),
+              ),
+            );
+          },
+          child: Container(
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.grey.shade300),
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [const SizedBox(height: 5), Text(product.name)],
+            ),
           ),
-          child: Center(child: Text("Shoes ${index + 1}")),
         );
       },
     );
